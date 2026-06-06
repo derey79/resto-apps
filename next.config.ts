dca73280680114f66012457d803b865351a19332
derey79 <devyreymond@gmail.com>
2026-06-06 19:26:20 +0700

    pindah ke project next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: "C:\\Users\\Derey\\Documents\\Derey's\\WPH-Bootcamp\\Github\\resto-apps-next",
+  },
 };
 
 export default nextConfig;
